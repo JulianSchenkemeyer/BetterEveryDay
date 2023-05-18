619ba340c74001486d7d84d4508a14a5beaa9150
Julian Schenkemeyer <julianschenkemeyer@fastmail.com>
2023-05-18 17:00:50 +0200

    init project

---
+++ BetterEveryDay/BetterEveryDay/BetterEveryDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterEveryDayApp.swift
+//  BetterEveryDay
+//
+//  Created by Julian Schenkemeyer on 18.05.23.
+//
+
+import SwiftUI
+
+@main
+struct BetterEveryDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterEveryDay/BetterEveryDay/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BetterEveryDay
+//
+//  Created by Julian Schenkemeyer on 18.05.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
