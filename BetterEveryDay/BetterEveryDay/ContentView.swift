d62ac67a155f4c91474deee4d4f1eeb5a4a6d885
Julian Schenkemeyer <julianschenkemeyer@fastmail.com>
2023-05-20 17:23:59 +0200

    Tryout logic for stopwatch

--- BetterEveryDay/BetterEveryDay/ContentView.swift
+++ BetterEveryDay/BetterEveryDay/ContentView.swift
@@ -6,21 +6,50 @@
 //
 
 import SwiftUI
+import Combine
+
+class TimerViewModel: ObservableObject {
+	private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+	private var timerSubscription: Cancellable?
+
+	@Published var time = Date()
+
+
+	func start() {
+		timerSubscription = timer.assign(to: \.time, on: self)
+	}
+
+	func stop() {
+		timerSubscription = nil
+	}
+}
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	@ObservedObject var viewModel: TimerViewModel = TimerViewModel()
+
+	var body: some View {
+		VStack {
+			Text(viewModel.time.formatted(.dateTime.minute(.twoDigits).second(.twoDigits)))
+
+			Button {
+				viewModel.start()
+			} label: {
+				Text("start")
+			}
+			.padding()
+
+			Button {
+				viewModel.stop()
+			} label: {
+				Text("stop")
+			}
+			.padding()
+		}
+	}
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//		ContentView(viewModel: <#TimerViewModel#>)
+//    }
+//}
