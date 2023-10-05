5c079241f6442983cff4d9ab3745a72dde7bd942
Julian Schenkemeyer <julian@jschenkemeyer.com>
2023-10-05 16:39:59 +0200

    Fix an issue with the SessionWithLimit implementation where the availableBreaktime was not calculated correctly

--- BetterEveryDay/BetterEveryDay/Model/Session/SessionWithLimit.swift
+++ BetterEveryDay/BetterEveryDay/Model/Session/SessionWithLimit.swift
@@ -8,15 +8,48 @@
 import Foundation
 
 
+final class BreaktimeTracker {
+    private (set) var breaktime: TimeInterval
+    private let limit: TimeInterval
+    
+    init(breaktime: TimeInterval, limit: TimeInterval) {
+        self.breaktime = breaktime
+        self.limit = limit
+    }
+    
+    func increaseBreaktime(by sessionLength: TimeInterval) {
+        breaktime = min(breaktime + sessionLength, limit)
+    }
+    
+    func decreaseBreaktime(by sessionLength: TimeInterval) { 
+        breaktime = breaktime - sessionLength
+    }
+}
+
 struct SessionWithLimit: SessionProtocol {
     var state: SessionState
-    var history: [PhaseMarker]
+    var history: [PhaseMarker] {
+        didSet {
+            guard let lastSession = history.last else { return }
+            
+            switch lastSession.name {
+            case .Focus:
+                let length = lastSession.length / 3
+                tracker.increaseBreaktime(by: length)
+            case .Pause:
+                tracker.decreaseBreaktime(by: lastSession.length)
+            default:
+                tracker.increaseBreaktime(by: 0)
+            }
+        }
+    }
     var started: Date
     
     let breakLimit: TimeInterval
+    var tracker: BreaktimeTracker
     
     var availableBreakTime: TimeInterval {
-        return min(total.focus / 3 - total.break, breakLimit)
+        return tracker.breaktime
     }
     
     init(breakLimit: TimeInterval) {
@@ -24,5 +57,6 @@ struct SessionWithLimit: SessionProtocol {
         self.history = []
         self.started = .now
         self.breakLimit = breakLimit
+        self.tracker = BreaktimeTracker(breaktime: 0, limit: breakLimit)
     }
 }
