946e7ffd22e3c74f56c3479457cd6816bc7be8f5
Julian Schenkemeyer <julian@jschenkemeyer.com>
2023-12-03 17:20:32 +0100

    Create the persistenceManager and make it available as environmentObject

--- BetterEveryDay/BetterEveryDay/BetterEveryDayApp.swift
+++ BetterEveryDay/BetterEveryDay/BetterEveryDayApp.swift
@@ -6,20 +6,31 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct BetterEveryDayApp: App {
     @StateObject private var notificationManager = EnvironmentManager.setupNotifications()
+    @StateObject private var persistenceManager = EnvironmentManager.setupPersistenceManager()
+    
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(persistenceManager.modelContainer)
         .environmentObject(notificationManager)
+        .environmentObject(persistenceManager)
     }
 }
+
 enum EnvironmentManager {
-    static func setupNotifications() -> NotificationManager {
+    @MainActor static func setupNotifications() -> NotificationManager {
         let service = NotificationService(notificationCenter: .current())
         return NotificationManager(notificationService: service)
+    }
+    
+    @MainActor static func setupPersistenceManager() -> SwiftDataPersistenceManager {
+        SwiftDataPersistenceManager()
     }
 }
