6e1652c1b47e8c16c6c1ea7151f39753bb1f8def
Julian Schenkemeyer <julian@jschenkemeyer.com>
2023-12-24 16:41:23 +0100

    Make ui adjustments for macOS

--- BetterEveryDay/BetterEveryDay/ContentView.swift
+++ BetterEveryDay/BetterEveryDay/ContentView.swift
@@ -9,9 +9,32 @@ import SwiftUI
 
 
 struct ContentView: View {
+#if os(macOS)
+    @State private var selected = "timer"
     
     var body: some View {
-        
+        NavigationSplitView(sidebar: {
+            List(selection: $selected) {
+                Label("Timer", systemImage: "clock")
+                    .tag("timer")
+                
+                Label("Settings", systemImage: "gear")
+                    .tag("settings")
+            }
+        }, detail: {
+            switch true {
+            case selected == "timer":
+                ThirdTimeView()
+            case selected == "settings":
+                SettingsView()
+            default:
+                Text("Please select a view")
+            }
+        })
+        .navigationTitle("Better Every Day")
+    }
+#else
+    var body: some View {
         TabView {
             NavigationStack {
                 ThirdTimeView()
@@ -28,6 +51,7 @@ struct ContentView: View {
             }
         }
     }
+#endif
 }
 
 
