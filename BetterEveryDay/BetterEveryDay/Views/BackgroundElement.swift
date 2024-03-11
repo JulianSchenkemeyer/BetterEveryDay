68c82774db1a48dd6bb5cb5b1f14501c30ebfc1b
Julian Schenkemeyer <julian@jschenkemeyer.com>
2024-03-11 16:30:28 +0100

    add an animated backgroundElement for the timer views

--- BetterEveryDay/BetterEveryDay/Views/BackgroundElement.swift
+++ BetterEveryDay/BetterEveryDay/Views/BackgroundElement.swift
@@ -7,58 +7,63 @@
 
 import SwiftUI
 
-struct BackgroundElement: View {
-    enum WanderingShadowAnimation: Int, CaseIterable {
-        case BottomRight, BottomLeft, TopLeft, TopRight
-        
-        var offsetX: CGFloat {
-            switch self {
-            case .BottomLeft, .TopLeft:
-                -1
-            case .BottomRight, .TopRight:
-                1
-            }
-        }
-        
-        var offsetY: CGFloat {
-            switch self {
-            case .BottomLeft, .BottomRight:
-                1
-            case .TopLeft, .TopRight:
-                -1
-            }
-        }
-        
-        var  radius: CGFloat {
-            switch self {
-            case .BottomLeft, .TopRight:
-                1
-            case .BottomRight, .TopLeft:
-                1
-            }
-        }
-        
-        var opacity: Double {
-            switch self {
-            case .BottomLeft, .TopRight:
-                0.4
-            case .BottomRight, .TopLeft:
-                0.4
-            }
-        }
-        
-        var animation: Animation {
-            .linear(duration: 0.9)
+enum WanderingShadowAnimation: Int, CaseIterable {
+    case BottomRight, BottomLeft, TopLeft, TopRight
+    
+    var offsetX: CGFloat {
+        switch self {
+        case .BottomLeft, .TopLeft:
+            -1
+        case .BottomRight, .TopRight:
+            1
         }
     }
     
+    var offsetY: CGFloat {
+        switch self {
+        case .BottomLeft, .BottomRight:
+            1
+        case .TopLeft, .TopRight:
+            -1
+        }
+    }
+    
+    var  radius: CGFloat {
+        switch self {
+        case .BottomLeft, .TopRight:
+            1
+        case .BottomRight, .TopLeft:
+            1
+        }
+    }
+    
+    var opacity: Double {
+        switch self {
+        case .BottomLeft, .TopRight:
+            0.4
+        case .BottomRight, .TopLeft:
+            0.4
+        }
+    }
+    
+    var animation: Animation {
+        .linear(duration: 0.9)
+    }
+}
+
+struct BackgroundElement: View {
     var size: CGFloat
+    var clockwise: Bool = true
+    
+    var animation: [WanderingShadowAnimation] {
+        clockwise ? WanderingShadowAnimation.allCases : WanderingShadowAnimation.allCases.reversed()
+    }
     
     var body: some View {
         Circle()
             .fill(.ultraThinMaterial)
             .frame(width: size, height: size)
-            .phaseAnimator(WanderingShadowAnimation.allCases) { content, phase in
+            .phaseAnimator(animation) { content, phase in
                 content
                     .shadow(color: .white.opacity(phase.opacity),
                             radius: phase.radius,
@@ -75,5 +80,5 @@ struct BackgroundElement: View {
 }
 
 #Preview {
-    BackgroundElement(size: 300)
+    BackgroundElement(size: 300, clockwise: false)
 }
