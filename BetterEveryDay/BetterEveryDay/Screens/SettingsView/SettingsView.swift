4885bb8704597c33c679bb35881386b99d393500
Julian Schenkemeyer <julian@jschenkemeyer.com>
2024-08-19 14:46:29 +0200

    add initial version of a slider to change the breaktime factor to settings

--- BetterEveryDay/BetterEveryDay/Screens/SettingsView/SettingsView.swift
+++ BetterEveryDay/BetterEveryDay/Screens/SettingsView/SettingsView.swift
@@ -11,21 +11,31 @@ struct SettingsView: View {
     
     let limits = [0, 180, 240, 300, 360, 420, 480, 540, 600]
     @AppStorage("breaktimeLimit") private var breaktimeLimit: Int = 0
+    @AppStorage("breaktimeFactor") private var breaktimeFactor = 3.0
     
     var body: some View {
         Form {
             Section {
-                
                 Picker("Limit Breaktime", selection: $breaktimeLimit) {
                     ForEach(limits, id: \.self) {
                         LimitOptionsLabel(value: ($0 / 60))
                     }
                 }
-                .padding(.vertical, 10)
-                    
+                
+                VStack(alignment: .leading) {
+                    Text("Factor:")
+                    Text("\(breaktimeFactor, format: .number) minutes work for 1 minute pause")
+                        .font(.callout)
+                        .foregroundStyle(.secondary)
+                        
+                    Slider(value: $breaktimeFactor, in: 1...5, step: 1) {
+                        Text("Factor")
+                    }
+                }
             } header: {
-                Text("Session")
+                Text("Session Settings")
             }
+            .padding(.vertical, 10)
             
             Section {
                 RequestNotificationView()
