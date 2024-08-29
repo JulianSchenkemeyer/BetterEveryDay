0dd465da8ad80e04f41d7de45f91cc412d915345
Julian Schenkemeyer <julian@jschenkemeyer.com>
2024-08-29 21:51:17 +0200

    implement functions to restore a running session

--- BetterEveryDay/BetterEveryDay/Manager/PersistenceManager.swift
+++ BetterEveryDay/BetterEveryDay/Manager/PersistenceManager.swift
@@ -21,12 +21,15 @@ protocol PersistenceManagerProtocol: ObservableObject {
     /// Finish the running session entry in the persistence layer, which sets the state of the entry to finished
     /// - Parameter session: ``Session``  to be persisted
     func finishSession(with session: Session)
+
+    func getLatestRunningSession() -> SessionController?
 }
 
 final class PersistenceManagerMock: PersistenceManagerProtocol {
     func insertSession(from sessionController: SessionController) { }
     func updateSession(with session: Session) { }
     func finishSession(with session: Session) { }
+    func getLatestRunningSession() -> SessionController? { nil }
 }
 
 final class SwiftDataPersistenceManager: PersistenceManagerProtocol {
@@ -86,5 +89,45 @@ final class SwiftDataPersistenceManager: PersistenceManagerProtocol {
         
         currentSession.state = "Finished"
         currentSession.availableBreak = session.availableBreak
+    }
+
+    @MainActor func getLatestRunningSession() -> SessionController? {
+        let running = SessionState.RUNNING.rawValue
+        let predicate = #Predicate<SessionData> { session in
+            session.state == running
+        }
+        let sorting = [SortDescriptor<SessionData>(\.started, order: .reverse)]
+        var fetchDescriptor = FetchDescriptor(predicate: predicate, sortBy: sorting)
+        fetchDescriptor.fetchLimit = 1
+        
+        do {
+            let unfinishedSession = try modelContainer.mainContext.fetch(fetchDescriptor)
+            guard let unfinishedSession = unfinishedSession.first else { return nil }
+            
+            currentSession = unfinishedSession
+            
+            return restoreSession(with: unfinishedSession)
+        } catch {
+            print("")
+            return nil
+        }
+    }
+    
+    func restoreSession(with data: SessionData) -> SessionController {
+        var sections = data.segments
+            .map { SessionSegment(category: SessionCategory(rawValue: $0.category)!, startedAt: $0.startedAt, finishedAt: $0.finishedAt) }
+            .sorted(using: [KeyPathComparator(\.startedAt, order: .forward)])
+        
+        if let last = sections.last, let finishedAt = last.finishedAt {
+            let category: SessionCategory = last.category == SessionCategory.Focus ? .Pause : .Focus
+            sections.append(.init(category: category, startedAt: finishedAt))
+        } else {
+            // In case there is no session segment saved yet
+            sections.append(.init(category: .Focus, startedAt: data.started))
+        }
+        
+        let session = Session(segments: sections, availableBreak: data.availableBreak, breaktimeLimit: data.breaktimeLimit, breaktimeFactor: data.breaktimeFactor)
+        
+        return SessionController(state: SessionState(rawValue: data.state)!, goal: data.goal, started: data.started, sections: session )
     }
 }
