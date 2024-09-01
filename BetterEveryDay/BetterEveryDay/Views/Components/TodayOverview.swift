a0cce54192884e2371f0ed668c65b15517fc0b85
Julian Schenkemeyer <julian@jschenkemeyer.com>
2024-09-01 08:49:53 +0200

    create TodayOverview file

---
+++ BetterEveryDay/BetterEveryDay/Views/Components/TodayOverview.swift
@@ -0,0 +1,19 @@
+//
+//  TodayOverview.swift
+//  BetterEveryDay
+//
+//  Created by Julian Schenkemeyer on 31.08.24.
+//
+
+import SwiftUI
+
+
+struct TodayOverview: View {
+    var body: some View {
+        Text("Today")
+    }
+}
+
+#Preview {
+    TodayOverview()
+}
