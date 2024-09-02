f7ca3e122815ea14c109fd016146c5bfeeb24a2c
Julian Schenkemeyer <julian@jschenkemeyer.com>
2024-09-02 21:13:13 +0200

    initial mockup with mock data for the TodayOverview

--- BetterEveryDay/BetterEveryDay/Views/Components/TodayOverview.swift
+++ BetterEveryDay/BetterEveryDay/Views/Components/TodayOverview.swift
@@ -6,12 +6,61 @@
 //
 
 import SwiftUI
-
+import Charts
 
 struct TodayOverview: View {
     var body: some View {
-        Text("Today")
+        VStack {
+            HStack {
+                Text("Daily Summary")
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Text(.now, format: .dateTime.day().month().year())
+                    .foregroundStyle(.secondary)
+            }
+            .frame(maxWidth: .infinity)
+            .font(.subheadline)
+
+            Divider()
+            
+            HStack(spacing: 10) {
+                VStack {
+                    Text("Sessions")
+                        .font(.caption.bold())
+                        .foregroundStyle(.secondary)
+                    
+                    Text("4")
+                        .font(.title3)
+                }
+                .frame(maxWidth: .infinity)
+                
+                Divider()
+                
+                VStack {
+                    Text("Total Length")
+                        .font(.caption.bold())
+                        .foregroundStyle(.secondary)
+                    
+                    Text("6:18")
+                        .font(.title3)
+                }
+                .frame(maxWidth: .infinity)
+            }
+            Divider()
+            
+            Spacer(minLength: 50)
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: 250)
+        .background {
+            RoundedRectangle(cornerRadius: 25.0)
+                .fill(.ultraThinMaterial)
+        }
+        .shadow(color: .black.opacity(0.2), radius: 5, x: 1, y: 1)
+        .padding()
     }
+        
 }
 
 #Preview {
