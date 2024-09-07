896a39a8b7a1dddf676d413a21197b9c0e29035b
Julian Schenkemeyer <julian@jschenkemeyer.com>
2024-09-07 17:26:14 +0200

    use card component on TodayOverview

--- BetterEveryDay/BetterEveryDay/Views/Components/TodayOverview.swift
+++ BetterEveryDay/BetterEveryDay/Views/Components/TodayOverview.swift
@@ -13,20 +13,7 @@ struct TodayOverview: View {
     var todaysSessions: [SessionData] = []
     
     var body: some View {
-        VStack {
-            HStack {
-                Text("Daily Summary")
-                    .bold()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                
-                Text(.now, format: .dateTime.day().month().year())
-                    .foregroundStyle(.secondary)
-            }
-            .frame(maxWidth: .infinity)
-            .font(.subheadline)
-
-            Divider()
-            
+        Card {
             HStack(spacing: 10) {
                 VStack {
                     Text("Sessions")
@@ -38,7 +25,7 @@ struct TodayOverview: View {
                 }
                 .frame(maxWidth: .infinity)
                 
-                Divider()
+                
                 
                 VStack {
                     Text("Total Length")
@@ -50,19 +37,7 @@ struct TodayOverview: View {
                 }
                 .frame(maxWidth: .infinity)
             }
-            Divider()
-            
-            Spacer(minLength: 50)
         }
-        .padding()
-        .frame(maxWidth: .infinity, maxHeight: 250)
-        .background {
-            RoundedRectangle(cornerRadius: 25.0)
-                .fill(.ultraThinMaterial)
-                .shadow(color: .black.opacity(0.15), radius: 2, x: 1, y: 1)
-                .shadow(color: .white.opacity(0.3), radius: 2, x: -1, y: -1)
-        }
-        
     }
     
     var totalLength: TimeInterval {
