ecf4441f1a4faaa8bac1f07d16f9bb4a777bc363
Julian Schenkemeyer <julian@jschenkemeyer.com>
2024-10-30 05:58:44 +0100

    use @Entry macro to create the persistenceManager Environment Key Value combination

--- BetterEveryDay/BetterEveryDay/Extensions/EnvironmentValues+Ext.swift
+++ BetterEveryDay/BetterEveryDay/Extensions/EnvironmentValues+Ext.swift
@@ -8,13 +8,6 @@
 import SwiftUI
 
 
-private struct PersistenceManagerKey: EnvironmentKey {
-    static let defaultValue: (any PersistenceManagerProtocol)? = nil
-}
-
 extension EnvironmentValues {
-    var persistenceManager: (any PersistenceManagerProtocol)? {
-        get { self[PersistenceManagerKey.self] }
-        set { self[PersistenceManagerKey.self] = newValue }
-    }
+    @Entry var persistenceManager:  (any PersistenceManagerProtocol)?
 }
