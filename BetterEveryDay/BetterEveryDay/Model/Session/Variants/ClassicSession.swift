503e320f5865c91942ca27c684d0634d4b477914
Julian Schenkemeyer <julian@jschenkemeyer.com>
2025-01-05 16:31:42 +0100

    add initial logic to ClassicSession variant

--- BetterEveryDay/BetterEveryDay/Model/Session/Variants/ClassicSession.swift
+++ BetterEveryDay/BetterEveryDay/Model/Session/Variants/ClassicSession.swift
@@ -10,16 +10,49 @@ import Foundation
 @Observable final class ClassicSession: SessionProtocol {
     var segments: [SessionSegment] = []
     var availableBreak: TimeInterval = 0
+    private var activeSegmentIndex: Int = 0
+    
+    init(segments: [SessionSegment],
+         availableBreak: TimeInterval = 0,
+         activeSegmentIndex: Int = 0) {
+        self.segments = segments
+        self.availableBreak = availableBreak
+        self.activeSegmentIndex = activeSegmentIndex
+    }
+    
     
     func getCurrent() -> SessionSegment? {
-        return segments.last
+        if activeSegmentIndex >= segments.count {
+            return nil
+        }
+        return segments[activeSegmentIndex]
     }
     
     func next(onFinishingSegment: OnFinishingSegmentClosure) {
+        guard var last = getCurrent() else {
+            return
+        }
         
+        last.finishedAt = Date.now
+        
+        if let onFinishingSegment {
+            onFinishingSegment(0, last)
+        }
+        
+        if activeSegmentIndex < segments.count {
+            activeSegmentIndex += 1
+        }
     }
     
     func endSession(onFinishingSegment: OnFinishingSegmentClosure) {
+        guard var last = getCurrent() else {
+            return
+        }
         
+        last.finishedAt = Date.now
+        
+        if let onFinishingSegment {
+            onFinishingSegment(0, last)
+        }
     }
 }
