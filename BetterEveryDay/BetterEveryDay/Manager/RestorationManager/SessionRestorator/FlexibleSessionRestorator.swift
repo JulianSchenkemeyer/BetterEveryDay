4eb194e204c7001375f816a84dc1543a9d7ec2c5
Julian Schenkemeyer <julian@jschenkemeyer.com>
2025-02-12 21:35:46 +0100

    split RestorationManager, RestorationFactory, Protocols, etc. into their  own files

---
+++ BetterEveryDay/BetterEveryDay/Manager/RestorationManager/SessionRestorator/FlexibleSessionRestorator.swift
@@ -0,0 +1,27 @@
+//
+//  FlexibleSessionRestorator.swift
+//  BetterEveryDay
+//
+//  Created by Julian Schenkemeyer on 12.02.25.
+//
+
+
+final class FlexibleSessionRestorator: SessionRestoratorProtocol {
+    func restore(_ data: SessionData, onRestoredSegments: (([SessionSegment]) -> Void)? = nil) -> LatestSessionData {
+        var segments = restoreSegments(data)
+        
+        if let last = segments.last, let finishedAt = last.finishedAt {
+            let category: SegmentCategory = last.category == SegmentCategory.Focus ? .Pause : .Focus
+            segments.append(.init(category: category, startedAt: finishedAt))
+        } else {
+            // In case there is no session segment saved yet
+            segments.append(.init(category: .Focus, startedAt: data.started))
+        }
+        
+        let session = FlexibleSession(segments: segments, availableBreak: data.availableBreak, breaktimeLimit: data.breaktimeLimit, breaktimeFactor: data.breaktimeFactor)
+        return (goal: data.goal,
+                started: data.started,
+                session: session,
+                state: SessionState(rawValue: data.state)! )
+    }
+}
---
+++ BetterEveryDay/BetterEveryDay/Manager/RestorationManager/SessionRestorator/SessionRestoratorProtocol.swift
@@ -0,0 +1,20 @@
+//
+//  SessionRestoratorProtocol.swift
+//  BetterEveryDay
+//
+//  Created by Julian Schenkemeyer on 12.02.25.
+//
+import Foundation
+
+
+protocol SessionRestoratorProtocol {
+    func restore(_ data: SessionData, onRestoredSegments: (([SessionSegment]) -> Void)?) -> LatestSessionData
+}
+
+extension SessionRestoratorProtocol {
+    func restoreSegments(_ data: SessionData) -> [SessionSegment] {
+        data.segments
+            .map { SessionSegment(category: SegmentCategory(rawValue: $0.category)!, startedAt: $0.startedAt, finishedAt: $0.finishedAt) }
+            .sorted(using: [KeyPathComparator(\.startedAt, order: .forward)])
+    }
+}
