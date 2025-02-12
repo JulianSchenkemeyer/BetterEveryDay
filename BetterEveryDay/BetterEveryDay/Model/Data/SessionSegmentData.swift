248053b3451e92946781c05937afb8679544556e
Julian Schenkemeyer <julian@jschenkemeyer.com>
2025-02-12 21:24:30 +0100

    split SessionData and SessionSegmentData into separate file

---
+++ BetterEveryDay/BetterEveryDay/Model/Data/SessionSegmentData.swift
@@ -0,0 +1,25 @@
+//
+//  SessionSegmentData.swift
+//  BetterEveryDay
+//
+//  Created by Julian Schenkemeyer on 12.02.25.
+//
+import Foundation
+import SwiftData
+
+
+@Model final class SessionSegmentData {
+    var session: SessionData?
+    var category: SessionCategory.RawValue
+    var startedAt: Date
+    var finishedAt: Date?
+    var duration: Double
+    
+    init(category: SessionCategory.RawValue, startedAt: Date, finishedAt: Date? = nil, duration: Double) {
+        self.session = nil
+        self.category = category
+        self.startedAt = startedAt
+        self.finishedAt = finishedAt
+        self.duration = duration
+    }
+}
