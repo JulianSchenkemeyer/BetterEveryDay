cdbf17aa4f6c9213cec4f54f887f021ec306b360
Julian Schenkemeyer <julian@jschenkemeyer.com>
2025-02-14 20:42:19 +0100

    update RestorationManager documentation

--- BetterEveryDay/BetterEveryDay/Manager/RestorationManager/RestorationManager.swift
+++ BetterEveryDay/BetterEveryDay/Manager/RestorationManager/RestorationManager.swift
@@ -9,10 +9,19 @@ import Foundation
 
 typealias RunningSessionData = (goal: String, started: Date, session: SessionProtocol, state: SessionState)
 
+/// Protocol describing the methods needed to be impelemented by a RestorationManager, which oversees the restoration of a Session..
 protocol RestorationManagerProtocol {
+    
+    /// Transforms the given data into a tuple (``RunningSessionData``) containing the session data
+    /// - Parameters:
+    ///   - data: ``SessionData``  of the session, which is to be restored
+    ///   - onRestoredSegments: Optional closure, which can be used to process restored segments. Can be used for example
+    ///   to persist currently untracked segments.
+    /// - Returns: Tuple containing the session data
     func restoreSessions(from data: SessionData, onRestoredSegments: (([SessionSegment]) -> Void)?) -> RunningSessionData
 }
 
+/// ``RestorationManagerProtocol`` implementation
 final class RestorationManager: RestorationManagerProtocol {
     private var factory = SessionRestoratorFactory()
     
