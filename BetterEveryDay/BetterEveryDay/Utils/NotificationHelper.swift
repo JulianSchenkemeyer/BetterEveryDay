60ec3073163996028f9a618811ea74bc9386549a
Julian Schenkemeyer <julian@jschenkemeyer.com>
2025-02-15 11:02:58 +0100

    update NotificationHelper documentation

--- BetterEveryDay/BetterEveryDay/Utils/NotificationHelper.swift
+++ BetterEveryDay/BetterEveryDay/Utils/NotificationHelper.swift
@@ -9,8 +9,12 @@ import Foundation
 import UserNotifications
 
 
+
+/// A helper class for managing notification permissions.
 final class NotificationHelper {
     
+    /// Checks the current notification permission status.
+    /// - Returns: A Boolean value indicating whether notifications are authorized.
     static func checkNotificationPermissionStatus() async -> Bool {
         guard let status = try? await UNUserNotificationCenter.current().requestAuthorization() else {
             return false
@@ -18,7 +22,11 @@ final class NotificationHelper {
         return status
     }
     
+    /// Requests notification permission from the user.
+    /// - Throws: An error if the request fails.
+    /// - Returns: A Boolean value indicating whether the permission was granted.
     static func requestNofificationPermission() async throws -> Bool {
         try await UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound])
     }
 }
+
