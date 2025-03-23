9753e320860da888e66e7b4dc26ced74934bcc6e
Julian Schenkemeyer <julian@jschenkemeyer.com>
2025-03-23 16:45:53 +0100

    transfer basic layout from modal to sheet

--- BetterEveryDay/BetterEveryDay/Views/Components/AddNewSessionSheet.swift
+++ BetterEveryDay/BetterEveryDay/Views/Components/AddNewSessionSheet.swift
@@ -9,8 +9,55 @@ import SwiftUI
 
 
 struct AddNewSessionSheet: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    @State private var selectedSessionVariant: SessionType = .flexible
+    @FocusState private var focusSessionGoalInput: Bool
+    
+    @Binding var sessionGoal: String
+    var onStartSession: (SessionType) -> Void
+    
     var body: some View {
-        Text("Add New Session")
+        NavigationStack {
+            VStack {
+                Picker("Select Session Type", selection: $selectedSessionVariant) {
+                    Text("Flexible").tag(SessionType.flexible)
+                    Text("Fixed").tag(SessionType.fixed)
+                }
+                .pickerStyle(.segmented)
+                
+                Divider()
+                
+                HStack {
+                    TextField("Your Goal for the Session",
+                              text: $sessionGoal,
+                              axis: .vertical)
+                    .lineLimit(1...)
+                    .font(.body)
+                    .focused($focusSessionGoalInput)
+                    .padding([.leading, .vertical], 10)
+                    .padding(.trailing, 4)
+                }
+                .frame(maxHeight: .infinity, alignment: .topLeading)
+
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                }
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Start Session") {
+                        
+                    }
+                    .primaryButtonStyle()
+                }
+            }
+            .padding(4)
+            .padding(.bottom, 32)
+        }
     }
 }
 
@@ -18,8 +65,9 @@ struct AddNewSessionSheet: View {
 #Preview {
     Text("Preview")
         .sheet(isPresented: .constant(true)) {
-            AddNewSessionSheet()
-                .presentationDetents([.medium])
+            AddNewSessionSheet(sessionGoal: .constant(""),
+                               onStartSession: {_ in })
+            .presentationDetents([.medium])
         }
     
 }
