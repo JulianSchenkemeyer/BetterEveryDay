724ff282907ef1cdce09d300db2f2ae00aa515db
Julian Schenkemeyer <julian@jschenkemeyer.com>
2025-05-11 16:12:59 +0200

    add a enum with icon names

---
+++ BetterEveryDay/BetterEveryDay/Constants/Icons.swift
@@ -0,0 +1,14 @@
+//
+//  Symbols.swift
+//  BetterEveryDay
+//
+//  Created by Julian Schenkemeyer on 09.05.25.
+//
+
+
+enum Icons {
+    static let todaysSessionsEmpty = "list.bullet"
+    
+    static let flexibleSession = "arrowshape.zigzag.right" //scribble.variable
+    static let fixedSession = "repeat"
+}
