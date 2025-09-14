5e5b5171a59a7b6f1147a7044c12998e4d351f88
Julian Schenkemeyer <julian@jschenkemeyer.com>
2025-09-14 21:59:38 +0200

    add legend and focus-pause-ratio indicator

--- BetterEveryDay/BetterEveryDay/Views/Charts/SessionOverviewChartView.swift
+++ BetterEveryDay/BetterEveryDay/Views/Charts/SessionOverviewChartView.swift
@@ -12,33 +12,57 @@ import Charts
 
 struct SessionOverviewChartView: View {
     var data: [SessionSegment]
+    
+    var totals: (focus: TimeInterval, pause: TimeInterval) {
+        data.reduce(into: (focus: .zero, pause: .zero)) { result, segment in
+            switch segment.category {
+            case .Focus:
+                result.focus += segment.duration
+            case .Pause:
+                result.pause += segment.duration
+            }
+        }
+    }
+    
+    var ratio: TimeInterval {
+        totals.focus / 3
+    }
         
     var body: some View {
-        Chart(data) { segment in
-            BarMark(
-                x:.value(
-                    "Duration",
-                    segment.duration
-                ),
-                y:.value("Category", segment.category.rawValue),
-            )
-            .cornerRadius(8, style: .continuous)
-            .foregroundStyle(by: .value("Category", segment.category.rawValue))
+        Chart {
+            ForEach(data) { segment in
+                BarMark(
+                    x: .value(
+                        "Duration",
+                        segment.duration
+                    ),
+                    y: .value("Category", segment.category.rawValue)
+                )
+                .cornerRadius(8, style: .continuous)
+                .foregroundStyle(by: .value("Category", segment.category.rawValue))
+            }
 
-            
+            RuleMark(x: .value("Marker", ratio))
+                .foregroundStyle(.secondary)
+                .lineStyle(StrokeStyle(lineWidth: 2, dash: [4]))
         }
         .chartXAxis(.hidden)
         .chartYAxis(.hidden)
         .chartLegend {
-            
+            VStack(alignment: .leading) {
+                Text("Focus: \(Duration.seconds(totals.focus), format: .units(allowed: [.hours, .minutes, .seconds], width: .narrow))")
+                Text("Pause: \(Duration.seconds(totals.pause), format: .units(allowed: [.hours, .minutes, .seconds], width: .narrow))")
+            }
+            .foregroundStyle(Color.secondary)
         }
-//        .chartScrollableAxes(.horizontal)
-        
     }
 }
 
 #Preview {
-    SessionOverviewChartView(data: Mockdata.sessionSegments + Mockdata.sessionSegments)
+    SessionOverviewChartView(
+        data: Mockdata.sessionSegments,
+    )
         .padding()
         .frame(height: 180)
 }
+
